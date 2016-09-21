0a15a0eb61acea5a080859e4829e317283295465
Jake LeBoeuf <dev@jakeleboeuf.com>
2016-09-20 21:51:55 -0400

    update to index.js for fastboot support

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = {
   name: 'ember-credit-card',
   included: function(app) {
     this._super.included(app);
-
-    app.import(app.bowerDirectory + '/card/lib/js/card.js');
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import(app.bowerDirectory + '/card/dist/card.js');
+    }
   }
 };
