900ea38e77b3bce2fa354a0ec1a0a38c7e0a1c84
Jake LeBoeuf <dev@jakeleboeuf.com>
2017-07-19 10:25:43 -0400

    wraps in guard

--- index.js
+++ index.js
@@ -3,10 +3,19 @@
 
 module.exports = {
   name: 'ember-credit-card',
-  included: function(app) {
-    this._super.included(app);
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import(app.bowerDirectory + '/card/dist/card.js');
-    }
+  var map = require('broccoli-stew').map;
+
+  treeForVendor(defaultTree) {
+    var browserVendorLib = new Funnel(app.bowerDirectory + '/card/dist/card.js');
+
+    browserVendorLib = map(browserVendorLib, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+
+    return new mergeTrees([defaultTree, browserVendorLib]);
   }
+
+  included() {
+    // this file will be loaded in FastBoot but will not be eval'd
+    app.import(app.bowerDirectory + '/card/dist/card.js');
+  }
+
 };
